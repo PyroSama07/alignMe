5f79ad0d1007fd1b861c32415d8ae569c342a630
PyroSama07 <rochakranjan1011@gmail.com>
2023-04-27 05:53:05 +0530

    auto shut after 10 rep

--- code/camera.py
+++ code/camera.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import exercise
+import time
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
@@ -71,7 +72,7 @@ def start_camera(selected_exercise):
 
             cv2.imshow('Mediapipe Feed', image)
 
-            if cv2.waitKey(10) & 0xFF == ord('q'):
+            if (counter == 10 and stage=='down') or cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
         cap.release()
